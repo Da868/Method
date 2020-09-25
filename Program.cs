d539649e9cea4308d7e4b1c889810617990c9802
Montarious Starks <montarious@usf.edu>
2020-09-24 23:53:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Author: Montarious Starks
+// Date:   09/24/2020
+// Comments: This program is to shwo the fact people are able to have the information
+
+using Microsoft.VisualBasic;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {      
+            // Creation of the string to the message
+            string name = Message();
+           
+            // Variabe created to Display the input
+            DisplayOutput(name);
+           
+        }
+
+        public static string Message()
+        {
+            // Question asked to the user
+            Console.WriteLine("Please Enter Your Name");
+            return Console.ReadLine();
+            
+        }
+
+
+          public static void DisplayOutput(string name)
+        {
+            // Answer given back to the user with imput
+            Console.WriteLine(" Hello " + name + "" + " - Go Bucs");
+            // Required to exit the program
+            Console.WriteLine("Press any key to exit the program.....");
+            Console.ReadLine();
+            
+        }
+    }
+
+}
